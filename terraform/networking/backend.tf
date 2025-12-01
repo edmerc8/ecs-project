82fc4ce3a01cc38a01252a2ac7fd84160df6d36f
evanmercurio <evan.mercurio@parivedasolutions.com>
2025-12-01 07:55:37 -0600

    added s3 backend for networking folder with outputs

---
+++ terraform/networking/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "ecs-project-state-bucket"
+    key          = "networking/terraform.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true # Use S3 native locking
+  }
+}
