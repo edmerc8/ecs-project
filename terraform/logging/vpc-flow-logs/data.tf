536f9e11a8587c624879c3d165df0d398e59c46f
evanmercurio <evan.mercurio@parivedasolutions.com>
2025-12-16 10:23:23 -0600

    removed unused data references

--- terraform/logging/vpc-flow-logs/data.tf
+++ terraform/logging/vpc-flow-logs/data.tf
@@ -1,15 +1,3 @@
-# Access outputs from Database Module
-data "terraform_remote_state" "database" {
-  backend = "s3"
-
-  config = {
-    bucket = "ecs-project-state-bucket"
-
-    key    = "database/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
-
 # Access outputs from Networking Module
 data "terraform_remote_state" "networking" {
   backend = "s3"
@@ -18,17 +6,6 @@ data "terraform_remote_state" "networking" {
     bucket = "ecs-project-state-bucket"
 
     key    = "networking/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
-
-data "terraform_remote_state" "iam" {
-  backend = "s3"
-
-  config = {
-    bucket = "ecs-project-state-bucket"
-
-    key    = "iam/terraform.tfstate"
     region = "us-east-2"
   }
 }
