a837b44e02c19eea5d3bd17b954cd06656bdbe1f
evanmercurio <evan.mercurio@parivedasolutions.com>
2025-12-20 12:49:50 -0600

    added cloudtrail logs to s3 and cloudwatch

--- terraform/iam/iam-roles.tf
+++ terraform/iam/iam-roles.tf
@@ -79,6 +79,55 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_secrets_manager_rd
 }
 
 
+resource "aws_iam_role" "cloudtrail_cloudwatch_log_delivery_role" {
+  name = "cloudtrail-cloudwatch-log-delivery-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name      = "cloudtrail-cloudwatch-log-delivery-role"
+    Project   = "ecs-fargate"
+    Owner     = "edm"
+    CreatedBy = "terraform"
+  }
+}
+
+resource "aws_iam_policy" "cloudtrail_cloudwatch_logs_access_policy" {
+  name = "cloudtrail_cloudwatch_logs-access-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "*" # Update to only provide access to the group needed for the project (may need to use variable for cloudtrail log group name)
+      }
+    ]
+  })
+}
+
+# Attaches Cloudwatch Logs access role to CloudTrail Log Delivery Role
+resource "aws_iam_role_policy_attachment" "cloudtrail_role_cloudwatch_logs_access" {
+  role       = aws_iam_role.cloudtrail_cloudwatch_log_delivery_role.name
+  policy_arn = aws_iam_policy.cloudtrail_cloudwatch_logs_access_policy.arn
+}
+
+
+
 # Provides access to RDS Credentials in Secrets Manager
 resource "aws_iam_policy" "ecs_secrets_access" {
   name        = "ecs-secrets-access"
--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -3,3 +3,8 @@ output "task_execution_role_arn" {
   description = "ARN for the ECS Task Execution Role"
   value       = aws_iam_role.ecs_execution_role.arn
 }
+
+output "cloudtrail_cloudwatch_log_delivery_role_arn" {
+  description = "ARN for the CloudTrail Log Delivery Role to CloudWatch"
+  value       = aws_iam_role.cloudtrail_cloudwatch_log_delivery_role.arn
+}
