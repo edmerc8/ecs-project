aea2690a4a52dda9169ef4d7a720014cdfac580e
evanmercurio <evan.mercurio@parivedasolutions.com>
2025-12-21 07:31:58 -0600

    enabled container insights and created log group

--- terraform/containers/ecs-cluster.tf
+++ terraform/containers/ecs-cluster.tf
@@ -3,9 +3,15 @@ This file contains the terraform code for our ECS cluster.
 This will be an ECS Fargate cluster that will run two types of containers.
 One will be for the frontend of the application and the other will be for the backend.
 The ECS Cluster will be spread across two availability zones for High Availability.
+Needs to run after logging module
 */
 
 resource "aws_ecs_cluster" "cluster" {
   name = "cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enhanced"
+  }
 }
 
--- terraform/logging/cloudtrail/cloudwatch-cloudtrail.tf
+++ terraform/logging/cloudtrail/cloudwatch-cloudtrail.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "cloudtrail_logs" {
   name = "cloudtrail-logs"
 
-  retention_in_days = 1
+  retention_in_days = 1 # Would be longer in PRD, just for cost savings now
 }
