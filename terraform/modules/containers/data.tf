ee64e849c1cbd2ae9a5a0140ab054fc27afdd3d4
evanmercurio <evan.mercurio@parivedasolutions.com>
2025-12-24 21:02:05 -0600

    set specific resources in iam policies to incorporate least privilege approach

--- terraform/modules/containers/data.tf
+++ terraform/modules/containers/data.tf
@@ -1,3 +1,7 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 data "aws_ecr_repository" "ecr_frontend_repo_url" {
   name = "ecs-project-private-repo-frontend"
 }
@@ -5,4 +9,3 @@ data "aws_ecr_repository" "ecr_frontend_repo_url" {
 data "aws_ecr_repository" "ecr_backend_repo_url" {
   name = "ecs-project-private-repo-backend"
 }
-
