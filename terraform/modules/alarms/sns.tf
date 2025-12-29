e0e32107ac77e6aacab8c214894f82465dbb93da
evanmercurio <evan.mercurio@parivedasolutions.com>
2025-12-29 12:25:11 -0600

    used variable for email

--- terraform/modules/alarms/sns.tf
+++ terraform/modules/alarms/sns.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "cloudwatch_alarm_topic" {
 resource "aws_sns_topic_subscription" "cloudwatch_alarm_email_subscriber" {
   topic_arn = aws_sns_topic.cloudwatch_alarm_topic.arn
   protocol  = "email"
-  endpoint  = "edmerc8@gmail.com"
+  endpoint  = var.alarm_email
 }
